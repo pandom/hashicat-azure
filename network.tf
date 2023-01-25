caab46f2434056ca19dfe24e2d662b873f43a3be
pandom <pandom@users.noreply.github.com>
2023-01-25 13:48:14 +1100

    Use For_each

--- network.tf
+++ network.tf
@@ -2,4 +2,5 @@ module "network" {
   source  = "app.terraform.io/burkey/network/azurerm"
   version = "5.0.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  use_for_each = false
 }
